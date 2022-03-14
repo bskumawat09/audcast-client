5ba42378ac90a33c0feefb51d5d9adff1afa48bc
Suresh Kumawat <bskumawat3132@gmail.com>
2022-03-14 21:17:54 +0530

    Handle cors

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,13 +2,14 @@ import { io } from "socket.io-client";
 
 const socketInit = () => {
 	const options = {
+		withCredentials: true,
 		"force new connection": true,
 		reconnectionAttempt: "Infinity",
 		timeout: 10000,
 		transports: ["websocket"]
 	};
 
-	return io(process.env.REACT_APP_BASE_URL, options);
+	return io("https://audcast.herokuapp.com", options);
 };
 
 export default socketInit;
